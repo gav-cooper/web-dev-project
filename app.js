cf70823a64585aa3078fa9a05b36d525c1ba46a5
Gavin Cooper <gavin.cooper@smail.astate.edu>
2022-03-03 20:42:10 -0600

    Updated user login

--- app.js
+++ app.js
@@ -11,14 +11,16 @@ app.use(express.json());
 // Require model
 const userModel = require("./Models/userModel")
 
-
 app.post("/users", async (req,res) => {
-    if (!req.body.username || !req.body.email || !req.body.password) {
+    if (req.body.username.includes("@") || !req.body.username || 
+        !req.body.email || !req.body.password) {
         return res.sendStatus(400);
     }
+
     const {username,password} = req.body;
     let {email} = req.body;
     email = email.toLowerCase();
+
     // User Model returns a promise that needs to be resolved
     if (!(await userModel.createUser(username,email,password))) {
         return res.sendStatus(409);
@@ -28,16 +30,17 @@ app.post("/users", async (req,res) => {
 
 app.post("/login", async (req,res) => {
     // Login using username or email
-    if (!(req.body.username || req.body.email) || !req.body.password) {
+    if (!req.body.value || !req.body.password) {
         return res.sendStatus(400);
     }
 
-    if (req.body.username){ // Login with username
-        const {username, password} = req.body;
-        const user = userModel.getUserByUsername(username);
+    if (!req.body.value.includes("@")){ // Login with username
+        const {value, password} = req.body;
+        const user = userModel.getUserByUsername(value);
         if (!user) {
             return res.sendStatus(400);
         }
+
         const {passwordHash} = user;
         if (await argon2.verify(passwordHash,password)) {
             // User login success
@@ -46,14 +49,16 @@ app.post("/login", async (req,res) => {
             // User login failure
             res.sendStatus(400);
         }
-    } else if (req.body.email) { // Login with email
-        let {email} = req.body;
-        email = email.toLowerCase();
+    } else if (req.body.value.includes("@")) { // Login with email
+        let {value} = req.body;
+        let email = value.toLowerCase();
         const {password} = req.body;
+
         const user = userModel.getUserByEmail(email);
         if (!user) {
             return res.sendStatus(400);
         }
+        
         const {passwordHash} = user;
         if (await argon2.verify(passwordHash,password)) {
             // User login success

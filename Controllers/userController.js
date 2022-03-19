23b5bd8e6c3f205ec421623596638dfaed389009
Gavin Cooper <gavin.cooper@smail.astate.edu>
2022-03-18 22:34:52 -0500

    Endpoints now call Controllers to reduce app.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -16,33 +16,82 @@ const userModel = require("../Models/userModel");
 /******************************************************************************/
 
 async function createNewUser(req, res){
-    const {username, password} = req.body;
-    userModel.addUser(username, password);
+    if (req.body.username.includes("@") || !req.body.username || 
+        !req.body.email || !req.body.password) {
+        return res.sendStatus(400);
+    }
+
+    const {password} = req.body;
+    let {username, email} = req.body;
+    username = username.toLowerCase();
+    email = email.toLowerCase();
+
+    // User Model returns a promise that needs to be resolved
+    if (!(await userModel.addUser(username,email,password))) {
+        return res.sendStatus(409);
+    }
     res.sendStatus(201);
 } 
 
 // Login
 async function login(req, res){
-    const {username, password} = req.body;
-    const user = userModel.getUserByUsername(username);
+    // Login using username or email
+    if (!req.body.value || !req.body.password) {
+        return res.sendStatus(400);
+    }
 
-    const {hash} = user;
-    if (await argon2.verify(hash, password)){
-        req.session.regenerate((err) => {
-            if (err){
-                console.error(err);
-                return res.sendStatus(500);  // internal server error
-            }
+    let {value} = req.body;
+    const {password} = req.body;
+    value = value.toLowerCase();
+    if (!req.body.value.includes("@")){ // Login with username
+        const user = userModel.getUserByUsername(value);
+        if (!user) {
+            return res.sendStatus(400);
+        }
 
-            req.session.user = {};
-            req.session.user.username = username;
-            req.session.user.userID = user.userID;
-            req.session.isLoggedIn = true;
-
-            res.sendStatus(200);
-        });
-    } else{
-        res.sendStatus(400);
+        const {passwordHash} = user;
+        if (await argon2.verify(passwordHash,password)) {
+            req.session.regenerate((err) => {
+                if (err){
+                    console.error(err);
+                    return res.sendStatus(500);  // internal server error
+                }
+    
+                req.session.user = {};
+                req.session.user.username = user.username;
+                req.session.user.userID = user.userID;
+                req.session.isLoggedIn = true;
+                console.log(req.session);
+                res.sendStatus(200);
+            });
+        } else {
+            // User login failure
+            res.sendStatus(400);
+        }
+    } else if (req.body.value.includes("@")) { // Login with email
+        const user = userModel.getUserByEmail(value);
+        if (!user) {
+            return res.sendStatus(400);
+        }
+        
+        const {passwordHash} = user;
+        if (await argon2.verify(passwordHash,password)) {
+            req.session.regenerate((err) => {
+                if (err){
+                    console.error(err);
+                    return res.sendStatus(500);  // internal server error
+                }
+    
+                req.session.user = {};
+                req.session.user.username = user.username;
+                req.session.user.userID = user.userID;
+                req.session.isLoggedIn = true;
+                res.sendStatus(200);
+            });
+        } else {
+            // User login failure
+            res.sendStatus(400);
+        }
     }
 }
 
--- app.js
+++ app.js
@@ -48,63 +48,9 @@ app.use(express.urlencoded({ extended: false }));
 const userController = require("./Controllers/userController");
 
 
-app.post("/users", async (req,res) => {
-    if (req.body.username.includes("@") || !req.body.username || 
-        !req.body.email || !req.body.password) {
-        return res.sendStatus(400);
-    }
-
-    const {username,password} = req.body;
-    let {email} = req.body;
-    email = email.toLowerCase();
-
-    // User Model returns a promise that needs to be resolved
-    if (!(await userModel.addUser(username,email,password))) {
-        return res.sendStatus(409);
-    }
-    res.sendStatus(201);
-});
-
-app.post("/login", async (req,res) => {
-    // Login using username or email
-    if (!req.body.value || !req.body.password) {
-        return res.sendStatus(400);
-    }
-
-    if (!req.body.value.includes("@")){ // Login with username
-        const {value, password} = req.body;
-        const user = userModel.getUserByUsername(value);
-        if (!user) {
-            return res.sendStatus(400);
-        }
-
-        const {passwordHash} = user;
-        if (await argon2.verify(passwordHash,password)) {
-            // User login success
-            res.sendStatus(200);
-        } else {
-            // User login failure
-            res.sendStatus(400);
-        }
-    } else if (req.body.value.includes("@")) { // Login with email
-        let {value} = req.body;
-        let email = value.toLowerCase();
-        const {password} = req.body;
-
-        const user = userModel.getUserByEmail(email);
-        if (!user) {
-            return res.sendStatus(400);
-        }
-        
-        const {passwordHash} = user;
-        if (await argon2.verify(passwordHash,password)) {
-            // User login success
-            res.sendStatus(200);
-        } else {
-            // User login failure
-            res.sendStatus(400);
-        }
-    }
-});
-
+app.post("/users", userController.createNewUser);
+app.post("/login", userController.login);
+app.get("/api/test", (req, res) => {
+    res.json({"user":req.session.user, "isLoggedIn":req.session.isLoggedIn});
+})
 module.exports = app;

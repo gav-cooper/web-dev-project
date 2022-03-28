ce41d95125004f8d7648b1431eee975337be143f
Gavin Cooper <gavin.cooper@smail.astate.edu>
2022-03-28 17:47:04 -0500

    storing date as an integer instead of text

--- Database/schema.sql
+++ Database/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS Posts (
     postID TEXT PRIMARY KEY,
     author TEXT NOT NULL,
     post TEXT NOT NULL,
-    date TEXT NOT NULL,
+    date INT NOT NULL,
     likes INTEGER NOT NULL DEFAULT 0 CHECK (likes >= 0),
     FOREIGN KEY(author) REFERENCES Users(username)
 );

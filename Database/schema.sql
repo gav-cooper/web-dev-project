b1bb5871bd6db88622facc9d2f34f325ffe89de5
Cody <cody.painter1@smail.astate.edu>
2022-04-01 15:13:18 -0500

    adding date column to Comments table

--- Database/schema.sql
+++ Database/schema.sql
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS Comments (
     commentID TEXT PRIMARY KEY,
     author TEXT UNIQUE NOT NULL,
     post TEXT UNIQUE NOT NULL,
+    date INT NOT NULL,
     likes INTEGER NOT NULL DEFAULT 0 CHECK (likes >= 0),
     FOREIGN KEY(author) REFERENCES Users(username),
     FOREIGN KEY(post) REFERENCES Posts(postID)

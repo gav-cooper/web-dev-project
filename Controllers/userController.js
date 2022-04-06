12e6d3206f0e60446db48cada399e25dcc63212d
Gavin Cooper <gavin.cooper@smail.astate.edu>
2022-04-06 00:30:38 -0500

    Wrote script for html account creation

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -15,8 +15,7 @@ const userModel = require("../Models/userModel");
 /******************************************************************************/
 
 async function createNewUser(req, res){
-    const {password} = req.body;
-    let {username, email} = req.body;
+    const {username, email, password} = req.body;
 
     // User Model returns a promise that needs to be resolved
     if (!(await userModel.addUser(username,email,password))) {
@@ -28,10 +27,9 @@ async function createNewUser(req, res){
 // Login
 async function login(req, res){
     // Login using username or email
-    let {value} = req.body;
-    const {password} = req.body;
-    
-    if (!req.body.value.includes("@")){ // Login with username
+    const {value, password} = req.body;
+
+    if (!value.includes("@")){ // Login with username
         const user = userModel.getUserByUsername(value);
         if (!user) {
             return res.sendStatus(400);
@@ -55,7 +53,7 @@ async function login(req, res){
             // User login failure
             res.sendStatus(400);
         }
-    } else if (req.body.value.includes("@")) { // Login with email
+    } else if (value.includes("@")) { // Login with email
         const user = userModel.getUserByEmail(value);
         if (!user) {
             return res.sendStatus(400);
---
+++ public/js/register.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const form = document.getElementById("createUserForm");
+
+form.addEventListener("submit", submitUserForm);
+
+async function submitUserForm (event) {
+    event.preventDefault();
+    const errorsContainer = document.querySelector("#errors");
+    errorsContainer.innerHTML = "";
+    
+    const body = getInputs();
+
+    try {
+        const response = await fetch("/register", {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": JSON.stringify(body)
+        });
+        if (response.ok) {      // Account created
+            window.location.href="index"; 
+
+        } else if (response.status === 400) {   // Input parameter error
+            const data = await response.json();
+            const errors = data.errors;
+            
+            for (const errorMsg of errors) {
+                console.error(errorMsg);
+                appendData(errorsContainer, errorMsg, "error");
+            }
+        } else if( response.status === 409) {  // Username/Email already in DB
+            clearInputs();
+            appendData(errorsContainer, "Username and/or Email is Taken!", "error");
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function getInputs() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    return {
+        username,
+        email,
+        password
+    }
+}
+
+function appendData(container, message, className) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = message;
+    paragraph.classList.add(className);
+    container.append(paragraph);
+}
+
+function clearInputs() {
+    document.getElementById("username").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";    
+}
+
+function timeoutButton() {
+    const button = document.getElementById("submit");
+    button.disabled = true;
+    setTimeout(() => {
+        button.disabled = false;
+    }, 10000)
+    return true;
+}
